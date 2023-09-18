deffd0552ec57cd3c8bd6fbe9664a57592d22c76
Jerry Wu <91348682+jwu114@users.noreply.github.com>
2023-09-18 01:52:33 -0400

    update

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   themeConfig: {
     editLink: false,
-    logo: 'logo.png',
+    logo: '/logo.png',
     lastUpdated: false,
     footer: `Copyright © 2023<br /> by Jiarui Wu`,
     nprogress: false,
@@ -12,11 +12,15 @@ export default defineConfig({
       github: 'https://github.com/jwu114',
       linkedin: 'https://www.linkedin.com/in/jiarui-jerry-wu/',
     },
-    nav: [{ title: 'Projects', link: 'projects' },
+    nav: [{ title: 'Projects', link: '/projects' },
           { title: 'Contact', children: [{title: 'jwu114@u.rochester.edu'},{title: 'w670699337@gmail.com'}]}],
   },
   locales: [{ id: 'en-US', name: 'English'}],
-  base: '/jwu114.github.io',
+  mode: 'site',
+  devServer: {
+    port: 1998
+  },
+  base: '/jwu114.github.io/',
   publicPath: '/jwu114.github.io/',
   exportStatic: {},
 });
