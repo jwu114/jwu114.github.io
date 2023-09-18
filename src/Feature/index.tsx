95434ed160ab9b4e7bfaec85bb0ea873b09f826c
Jerry Wu <91348682+jwu114@users.noreply.github.com>
2023-09-18 00:29:05 -0400

    deleted all

--- src/Feature/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './index.less';
-
-export default ({ src, alt, width, title }: { src: string, alt: string, width: string, title: string }) =>
-  <div className='feature'>
-    <img src={src} alt={alt} width={width}/>
-    <p> {title} </p>
-  </div>
